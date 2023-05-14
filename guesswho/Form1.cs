34ec9bc10a1730d2bdb8e7bb2b8accfde5c7de4d
MatusBudos <91258409+MatusBudos@users.noreply.github.com>
2023-05-14 18:38:04 +0200

    Prepojenie databazy (done)

--- guesswho/Form1.cs
+++ guesswho/Form1.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace guesswho
 {
     public partial class Form1 : Form
@@ -16,6 +18,7 @@ namespace guesswho
         {
             InitializeComponent();
         }
+
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -28,7 +31,7 @@ namespace guesswho
             GameScreen hraciaplocha = new GameScreen();
             hraciaplocha.Closed += (s, args) => this.Close();
             hraciaplocha.Show();
-            
+
         }
 
         private void rules(object sender, EventArgs e)
@@ -37,7 +40,25 @@ namespace guesswho
             Help pravidla = new Help();
             pravidla.Closed += (s, args) => this.Close();
             pravidla.Show();
-            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            MySqlConnection connection = new MySqlConnection("SERVER=usa.vybrat.eu;PORT=3306;DATABASE=c43guesswho;UID=c43budos;PASSWORD=BziBy_aU7");
+            connection.Open();
+            if (connection.State == ConnectionState.Open)
+            {
+                button3.Text = "Success";
+                button3.BackColor = Color.Green;
+            }
+            else if (connection.State == ConnectionState.Closed)
+            {
+                button3.Text = "Not succes";
+                button3.BackColor = Color.Red;
+            }
+
         }
     }
 }
